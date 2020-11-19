b0cde00e0dd96423a65e5369421ccccae6aad048
u1Ys <u1@berlb.net>
2020-11-19 23:02:07 +0900

    Increment ordered-list numbers after `markdown-meta-return' position

--- markdown-meta-return.el
+++ markdown-meta-return.el
@@ -3,7 +3,7 @@
 like `org-meta-return'. (heading, unorded list, ordered list)"
   (interactive)
   (let*
-      ((this-line (buffer-substring (line-beginning-position) (line-end-position)))
+      ((this-line (markdown-meta-return-this-line))
        (new-line (cond
 		  ;; heading
 		  ((string-match "^\\(#+ \\)" this-line)
@@ -12,12 +12,8 @@ like `org-meta-return'. (heading, unorded list, ordered list)"
 		  ((string-match "^\\( *- \\)" this-line)
 		   (match-string 1 this-line))
 		  ;; ordered list
-		  ((string-match "^\\( *\\)\\([0-9]+\\)\\. " this-line)
-		   (concat (match-string 1 this-line)
-			   ;; new number = current number + 1
-		  	   (number-to-string (+ 1
-	    	  				(string-to-number (match-string 2 this-line))))
-		  	   ". "))
+		  ((markdown-meta-return-match-ordered-list this-line)
+		   (markdown-meta-return-ordered-list))
 		  (t "")
 		  )))
     (save-excursion (open-line '1))
@@ -25,5 +21,55 @@ like `org-meta-return'. (heading, unorded list, ordered list)"
     (next-line)
     (insert new-line)
     ))
+
+
+(defun markdown-meta-return-this-line ()
+  (buffer-substring-no-properties (line-beginning-position) (line-end-position))
+  )
+
+
+(defun markdown-meta-return-match-ordered-list (line)
+  (string-match "^\\( *\\)\\([0-9]+\\)\\. " line)
+  )
+
+
+(defun markdown-meta-return-ordered-list ()
+  ;; replace continuous ordered list
+  (save-excursion
+    (next-line)
+    (let
+	((this-line (markdown-meta-return-this-line)))
+      (while (markdown-meta-return-match-ordered-list this-line)
+	;; replace line number to incremented one
+	(replace-string
+	 this-line
+	 (markdown-meta-return-ordered-list-string this-line)
+	 nil
+	 (line-beginning-position) (line-end-position)
+	 )
+	;; increment sequence
+	(next-line)
+	(setq this-line (markdown-meta-return-this-line))
+	))
+    )
+  ;; return new string of this-line
+  (let
+      ((this-line (markdown-meta-return-this-line)))
+    (markdown-meta-return-match-ordered-list this-line)
+    (markdown-meta-return-ordered-list-string this-line)
+    ))
+
+
+(defun markdown-meta-return-ordered-list-string (str)
+  "NOTICE: this function should be used after
+`markdown-meta-return-match-ordered-list'"
+  (concat
+   ;; white space
+   (match-string 1 str)
+   ;; new number = current number + 1
+   (number-to-string (+ 1
+			(string-to-number (match-string 2 str))))
+   ". "))
+
 
 (provide 'markdown-meta-return)
